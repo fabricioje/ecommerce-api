8dcdd90689b1022d969722d8c5d721f3266b8e30
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-06-04 08:39:25 -0300

    Configurado os testes com RSpec
    * Instado as gems rspec-rails, factory_bot_rails e shoulda-matchers
    * Adicionado e configurado os arquivos rails_help.rb e spec_helper.rb

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,3 @@
+Rspec.configure do |config|
+    config.include FactoreyBot::Sintax::Methods
+end
---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,6 @@
+Soulda::Matchers.configure do |config|
+    config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+    end
+end
