156b7689b0dcd7bc9269bb0488bdd73a0dd5d1b5
Fabrício J. Silva <fabricio_fx@hotmail.com>
2021-10-19 17:42:30 -0300

    Validando uso do Cupom

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,12 +1,19 @@
 class Coupon < ApplicationRecord
-    include LikeSearchable
-    include Paginatable
-    
-    validates :name, presence: true
-    validates :code, presence: true, uniqueness: { case_sensitive: false }
-    validates :status, presence: true
-    validates :discount_value, presence: true, numericality: { greater_than: 0 }
-    validates :due_date, presence: true, future_date: true
+  class InvalidUse < StandardError; end
+
+  include LikeSearchable
+  include Paginatable
   
-    enum status: { active:1,  inactive: 2 }
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
+  validates :status, presence: true
+  validates :discount_value, presence: true, numericality: { greater_than: 0 }
+  validates :due_date, presence: true, future_date: true
+
+  enum status: { active:1,  inactive: 2 }
+
+  def validate_use!
+    raise InvalidUse unless self.active? && self.due_date >= Time.now
+    true
   end
+end
